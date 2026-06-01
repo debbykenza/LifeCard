6def11dcc650b132a763361600afbdbc9600f962
Debby Kenza <nicouebeglahkenza@gmail.com>
2026-06-01 22:22:15 +0200

    review new config url data

--- src/lib/server/db/index.ts
+++ src/lib/server/db/index.ts
@@ -3,8 +3,8 @@ import { createClient } from '@libsql/client';
 import * as schema from './schema';
 import { env } from '$env/dynamic/private';
 
-// if (!env.DATABASE_URL) throw new Error('DATABASE_URL is not set');
+//if (!env.DATABASE_URL) throw new Error('DATABASE_URL is not set');
 
-// const client = createClient({ url: env.DATABASE_URL });
+const client = createClient({ url: env.DATABASE_URL? env.DATABASE_URL : '' });
 
-// export const db = drizzle(client, { schema });
+export const db = drizzle(client, { schema });
